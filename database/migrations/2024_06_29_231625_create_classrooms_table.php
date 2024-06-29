d08658a28b07dbd155cb4d3fe9a6c6a5185a6a93
guvanchxh <maladisatabay356@gmail.com>
2024-06-30 04:51:19 +0500

    0.0.2

--- database/migrations/2024_06_29_231625_create_classrooms_table.php
+++ database/migrations/2024_06_29_231625_create_classrooms_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('location_id')
+                ->constrained('locations');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_29_231715_create_branches_table.php
+++ database/migrations/2024_06_29_231715_create_branches_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->date('birthday');
+            $table->enum('gender', ['male', 'female']);
             $table->timestamps();
         });
     }
